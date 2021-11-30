bf9b60d983d75e7719b00ca45dd0f571a1a05a16
liviabelirocha <liviabelirocha@outlook.com>
2021-11-30 11:20:39 -0300

    header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import styles from "./styles.module.scss";
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <h1>lib.news</h1>
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,6 @@ export default function Home() {
       <Head>
         <title>Início | lib.news</title>
       </Head>
-      <h1>Hello world!</h1>
     </>
   );
 }
