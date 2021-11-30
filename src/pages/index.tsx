4b653e2ed2a0bd7db6335e64292d8329f0f409b3
liviabelirocha <liviabelirocha@outlook.com>
2021-11-30 12:32:09 -0300

    feat: stripe get price

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
 import Head from "next/head";
+import { GetServerSideProps } from "next";
 
 import { SubscribeButton } from "../components/SubscribeButton";
 
 import styles from "../styles/home.module.scss";
 
-export default function Home() {
+import { stripe } from "../services/stripe";
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -19,10 +29,10 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />{" "}
-            <span>for R$9,90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="girl coding" />
@@ -30,3 +40,21 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1K1XwZC9627s8ysgNwlmKTsL");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
